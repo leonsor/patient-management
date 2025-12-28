89ea1ec3dc3bd3267bf06e161249cfda4eba9a7c
leonsor <34835973+leonsor@users.noreply.github.com>
2025-12-28 20:10:26 +0100

    Implementing update patient

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,9 +1,14 @@
 package com.pm.patientservice.service;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
+import com.pm.patientservice.exception.PatientNotFoundException;
+
 import org.springframework.stereotype.Service;
 
 import com.pm.patientservice.PatientMapper;
@@ -36,5 +41,21 @@ public class PatientService {
 		Patient newPatient = patientRepository.save(
 				PatientMapper.toModel(patientRequestDTO));
 		return PatientMapper.toDTO(newPatient);
+	}
+	
+	public PatientResponseDTO updatePatient(UUID id,
+			PatientRequestDTO patientRequestDTO) {
+		
+		Patient patient = patientRepository
+				.findById(id).
+				orElseThrow(() -> new PatientNotFoundException("Patient not find with ID:" + id));
+		
+		patient.setName(patientRequestDTO.getName());
+		patient.setAddress(patientRequestDTO.getAddress());
+		patient.setEmail(patientRequestDTO.getEmail());
+		patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+		Patient updatedPatient = patientRepository.save(patient);
+		return PatientMapper.toDTO(updatedPatient);
 	}
 }
